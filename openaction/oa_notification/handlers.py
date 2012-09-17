990b7f436758d8cdc18f1a8a8f9973f157f93ccb
Matteo Micheletti <matteo.micheletti@befair.it>
2012-09-17 16:19:27 +0200

    Updated Openaction installation file.
    Committed oa_notification/handlers.py for completeness (since it is a new file.)

---
+++ openaction/oa_notification/handlers.py
@@ -0,0 +1,44 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver 
+
+from askbot.models import Post
+from notification.models import send
+
+@receiver(pre_save, sender=Post)
+def notify_add_blog_post(sender, **kwargs):
+    """ Notify to Action referrers and followers that a new post 
+    has been added to the Action blog
+
+    #1 set the recipients (the receivers)
+    #2 send notification(notification.send)
+    
+    Possible options:
+    * 'now=True and queue=True': error
+    * 'now=True and queue=False': send notice immediately by passing 'now=True' to 
+    send function
+    * 'queue=True and now=False': queue the notice in order to send it later
+    * 'now=False and queue=False'(default): let the default notification 
+    configuration decide
+
+    QUESTION: should we pass the referrer who created the blog post as the 
+    sender of the notice (in sender=)?
+    """
+
+    post = kwargs['instance']
+
+    if post.is_answer():
+        action = post.thread.action
+
+        referrers = action.referrers
+        followers = action.thread.followed_by.objects.all()
+        # recipients
+        users = referrers.extend(followers)
+        
+        send(users=users, 
+            label="referrer_comment_same_blog_post", 
+            extra_context=None, 
+            on_site=True, 
+            sender=None, 
+            now=True
+        )
+ 
