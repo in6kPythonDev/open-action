34c169140122741ec880d10a237caa35ef2ba62e
Luca Ferroni <luca@befair.it>
2012-09-21 12:33:08 +0200

    Tuned template tags. Imported askbot consts for activity types

--- openaction/action/templatetags/action_tags.py
+++ openaction/action/templatetags/action_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from django.template import defaultfilters as filters
 
 
 register = template.Library()
@@ -20,12 +21,12 @@ def html_action_item(action):
     """Return html snippet for an action item."""
 
     html = """
-<div class="action-item">
+<li class="action-item">
     <div class="action-author">%(author)s</div>
     <div class="action-title">%(action)s</div>
     <div class="action-geonames">%(geonames)s</div>
     <div class="action-categories">%(categories)s</div>
-</div>
+</li>
 """ % {
         'author' : html_render_resource(action.author),
         'action' : html_render_resource(action),
@@ -39,35 +40,43 @@ def html_blogpost_item(blogpost):
     """Return html snippet for a blog post item."""
 
     html = """
-<div class="blogpost-item">
-    <div class="blogpost-author">%(blogpost_author)s</div>
-    <div class="blogpost-title">%(blogpost)s</div>
-    <div class="blogpost-action">%(action)s</div>
-    <div class="blogpost-ncomments">ha ricevuto %(ncomments)s commenti</div>
-</div>
+<li class="blogpost-item">
+    <div class="blogpost-action">%(action_title)s</div>
+    <div class="blogpost-title">%(blogpost_title)s</div>
+    <div class="blogpost-subtitle">%(blogpost_date)s - %(blogpost_author)s - %(ncomments)s commenti</div>
+    <div class="blogpost-excerpt">%(blogpost_excerpt)s</div>
+</li>
 """ % {
-        'blogpost_author' : html_render_resource(blogpost.author),
-        'blogpost' : html_render_resource(blogpost),
-        'action' : html_render_resource(blogpost.action),
+        'action_title' : blogpost.action,
+        'blogpost_title' : blogpost.title,
+        'blogpost_date' : blogpost.added_at,
+        'blogpost_author' : blogpost.author,
         'ncomments' : blogpost.comments.count(),
+        'blogpost_excerpt': filters.truncatewords(blogpost.html(), 300),
     }
     return html
 
 @register.simple_tag
-def html_activity_item(blogpost):
+def html_activity_item(activity):
     """Return html snippet for an activity item."""
+
+    extra_content = ""
+    if activity.activity_type == askbot_extensions_consts.TYPE_ACTIVITY_ANSWER:
+        extra_content = activity.content_object.title
 
     html = """
 <div class="activity-item">
-    <div class="activity-author">%(activity_user)s il %(activity_date)s</div>
+    <div class="activity-author">il %(activity_date)s %(activity_user)s</div>
     <div class="activity-type">ha %(activity_type)s</div>
     <div class="activity-action">%(action)s</div>
+    <div class="activity-extra">%(extra_content)s</div>
 </div>
 """ % {
         'activity_user' : activity.user,
         'activity_date' : activity.active_at,
-        'activity_type' : const.ACTIVITY_TYPE_DISPLAY_D[activity.activity_type],
+        'activity_type' : askbot_extensions_consts.ACTIVITY_TYPE_DISPLAY_D[activity.activity_type],
         'action' : activity.content_object.action,
+        'extra_content' : extra_content,
     }
     return html
 
---
+++ openaction/askbot_extensions/consts.py
@@ -0,0 +1,64 @@
+
+from askbot.const import *
+
+OA_TYPE_ACTIVITY_SET_VICTORY = 100
+
+TYPE_ACTIVITY_CHOICES = (
+
+    (OA_TYPE_ACTIVITY_SET_VICTORY, "impostato la vittoria sull'azione"),
+
+    # TODO: Matteo or Antonio: rinominare la descrizione
+    (TYPE_ACTIVITY_ASK_QUESTION, _('asked a question')),
+    (TYPE_ACTIVITY_ANSWER, _('answered a question')),
+    (TYPE_ACTIVITY_COMMENT_QUESTION, _('commented question')),
+    (TYPE_ACTIVITY_COMMENT_ANSWER, _('commented answer')),
+    (TYPE_ACTIVITY_UPDATE_QUESTION, _('edited question')),
+    (TYPE_ACTIVITY_UPDATE_ANSWER, _('edited answer')),
+    (TYPE_ACTIVITY_PRIZE, _('received badge')),
+    (TYPE_ACTIVITY_MARK_ANSWER, _('marked best answer')),
+    (TYPE_ACTIVITY_VOTE_UP, _('upvoted')),
+    (TYPE_ACTIVITY_VOTE_DOWN, _('downvoted')),
+    (TYPE_ACTIVITY_CANCEL_VOTE, _('canceled vote')),
+    (TYPE_ACTIVITY_DELETE_QUESTION, _('deleted question')),
+    (TYPE_ACTIVITY_DELETE_ANSWER, _('deleted answer')),
+    (TYPE_ACTIVITY_MARK_OFFENSIVE, _('marked offensive')),
+    (TYPE_ACTIVITY_UPDATE_TAGS, _('updated tags')),
+    (TYPE_ACTIVITY_FAVORITE, _('selected favorite')),
+    (TYPE_ACTIVITY_USER_FULL_UPDATED, _('completed user profile')),
+    (TYPE_ACTIVITY_EMAIL_UPDATE_SENT, _('email update sent to user')),
+    (
+        TYPE_ACTIVITY_UNANSWERED_REMINDER_SENT,
+        _('reminder about unanswered questions sent'),
+    ),
+    (
+        TYPE_ACTIVITY_ACCEPT_ANSWER_REMINDER_SENT,
+        _('reminder about accepting the best answer sent'),
+    ),
+    (TYPE_ACTIVITY_MENTION, _('mentioned in the post')),
+    (
+        TYPE_ACTIVITY_CREATE_TAG_WIKI,
+        _('created tag description'),
+    ),
+    (
+        TYPE_ACTIVITY_UPDATE_TAG_WIKI,
+        _('updated tag description')
+    ),
+    (TYPE_ACTIVITY_MODERATED_NEW_POST, _('made a new post')),
+    (
+        TYPE_ACTIVITY_MODERATED_POST_EDIT,
+        _('made an edit')
+    ),
+    (
+        TYPE_ACTIVITY_CREATE_REJECT_REASON,
+        _('created post reject reason'),
+    ),
+    (
+        TYPE_ACTIVITY_UPDATE_REJECT_REASON,
+        _('updated post reject reason')
+    ),
+    (
+        TYPE_ACTIVITY_VALIDATION_EMAIL_SENT,
+        'sent email address validation message'#don't translate, internal
+    ),
+
+)
