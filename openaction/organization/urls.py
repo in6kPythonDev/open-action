5beb9cf2d1bb17964e212c0c7480411e7af744b4
Luca Ferroni <luca@befair.it>
2012-09-29 08:37:53 +0200

    Review of organization application

--- openaction/organization/urls.py
+++ openaction/organization/urls.py
@@ -4,16 +4,20 @@ except ImportError as e:
     # Using Django < 1.4
     from django.conf.urls.defaults import patterns, url
 
-import organization.views as organization_views
+import organization.views as org_views
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)/$', organization_views.OrgView.as_view(),
+    url(r'^(?P<pk>\d+)/$', org_views.OrgView.as_view(),
         name='org-detail'
     ),
-    url(r'^(?P<pk>\d+)/follow/$', organization_views.UserFollowOrgView.as_view(),
+    url(r'^(?P<pk>\d+)/follow/$', org_views.UserFollowOrgView.as_view(),
         name='org-user-follow'
     ),
-    url(r'^(?P<pk>\d+)/represent/$', organization_views.UserRepresentOrgView.as_view(),
-        name='org-user-represent'
-    ),
+    #KO: we must not enable this feature through web POSTs
+    #KO: if we would like to implement such a thing we could wonder about
+    #TODOFUTURE: a user create an action_request.ActionRequest asking the staff (null recipient)
+    #TODOFUTURE: to be set as representative for an organization
+    #KO: url(r'^(?P<pk>\d+)/represent/$', org_views.UserRepresentOrgView.as_view(),
+    #KO:    name='org-user-represent'
+    #KO:),
 )
