e5626beaa20c2c5ed1f33472d2af2aece0108ab5
Luca Ferroni <luca@befair.it>
2012-10-01 09:02:04 +0200

    STUB: adapted users.views from OpenMunicipio to OpenAction.
    Context to be completed by Matteo

--- openaction/users/views.py
+++ openaction/users/views.py
@@ -3,12 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models import Count
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
-from open_municipio.acts.models import Deliberation, Motion, Interpellation, Emendation, Agenda, Interrogation
-from open_municipio.locations.models import Location
-from open_municipio.monitoring.models import Monitoring
-from open_municipio.people.models import Person, GroupCharge
-from open_municipio.taxonomy.models import Category, Tag
-from open_municipio.users.models import UserProfile
+
+from users.models import UserProfile
 
 class UserDetailView(DetailView):
 
@@ -17,7 +13,6 @@ class UserDetailView(DetailView):
         # object lookup using username
         object = User.objects.get(username=self.kwargs['username'])
 
-        # Return the object
         return object
 
 class UserProfileDetailView(DetailView):
@@ -30,21 +25,16 @@ class UserProfileDetailView(DetailView):
         # object lookup using username
         object = UserProfile.objects.get(user__username=self.kwargs['username'])
 
-        # Return the object
         return object
 
     def get_context_data(self, **kwargs):
         # call the base implementation first to get a context
         context = super(UserProfileDetailView, self).get_context_data(**kwargs)
 
-        context['act_monitoring_list'] = calculate_top_monitorings(
-            Deliberation, Motion, Interpellation, Emendation, Agenda, Interrogation,
-            user= self.object.user
-        )
-
-        context['politician_monitoring_list'] = [el.content_object for el in calculate_top_monitorings(Person, qnt=None, user=self.object.user)]
-
-        context['topic_monitoring_list'] = [el.content_object for el in calculate_top_monitorings(Tag,Category,Location, qnt=None, user=self.object.user)]
+        context['voted_actions'] = "TODO"
+        context['friends'] = "TODO"
+        context['followed_orgs'] = "TODO"
+        context['represented_orgs'] = "TODO"
 
         return context
 
@@ -58,22 +48,11 @@ class UserProfileListView(ListView):
         context = super(UserProfileListView, self).get_context_data(**kwargs)
 
         context.update({
-            # TODO if a person not have a institution_charge...?
-            'top_monitorized_politicians': [p.content_object.all_institution_charges[0] for p in calculate_top_monitorings(Person,qnt=3)],
-            'top_monitorized_topics': [el.content_object for el in calculate_top_monitorings(Tag,Category,Location)],
-            'top_monitorized_acts': [el.content_object for el in calculate_top_monitorings(Deliberation, Motion, Interpellation, Emendation, Agenda, Interrogation)],
+            'top_voted_actions': ["TODO"][:5],
+            'top_active_topics': ["TODO"][:5],
+            'top_active_locations': ["TODO"][:5],
         })
         return context
 
 
 
-def calculate_top_monitorings(*models, **kwargs):
-    query = dict(content_type__in=[ContentType.objects.get_for_model(m).pk for m in models]) if len(models) > 1 else dict(content_type=ContentType.objects.get_for_model(models[0]).pk)
-    if kwargs.get('user'):
-       query['user'] = kwargs.get('user')
-    return [
-        el for el in Monitoring.objects
-                 .filter(**query).select_related()
-                 .annotate(n_monitoring=Count('object_pk'))
-                 .order_by('-n_monitoring')[:kwargs.get('qnt',5)]
-    ]
