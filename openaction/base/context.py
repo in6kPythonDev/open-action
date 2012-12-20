89a7c74a1dd129c7e4e28a2fba5255e3d4ca9485
Daniele <joke2k@gmail.com>
2012-12-20 13:13:38 +0100

    add login page

--- openaction/base/context.py
+++ openaction/base/context.py
@@ -6,5 +6,7 @@ def global_context(request):
     context['main_arguments'] = ActionCategory.objects.all()
     context['main_locations'] = Geoname.objects.all()
     context['LESS_DEBUG'] = settings.LESS_DEBUG
+    context['LOGIN_URL'] = settings.LOGIN_URL
+    context['LOGOUT_URL'] = settings.LOGOUT_URL
 
     return context
