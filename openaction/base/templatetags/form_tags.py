bcdecbc31e5ae2d151eaa46197b824152f63ef58
Daniele <joke2k@gmail.com>
2012-12-20 17:28:34 +0100

    add form_tags template tags

---
+++ openaction/base/templatetags/form_tags.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('tags/form_field_text.html')
+def form_field_text(formField):
+    return {
+        'field': formField
+    }
+
+@register.inclusion_tag('tags/form_field_checkbox.html')
+def form_field_checkbox(formField):
+    return {
+        'field': formField
+    }
+
+@register.inclusion_tag('tags/form_field_text.html')
+def form_field_password(formField):
+    return {
+        'field': formField,
+        'type': 'password',
+        'empty_value': True,
+    }
